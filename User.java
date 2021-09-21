914aca4ee5e7a6ce1b2c987462d08c407f65d3bd
Ram Yadav <ramyadav.now@gmail.com>
2021-09-21 22:34:24 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.app.model;
+
+public class User {
+	private String firstName;
+	private Int id;
+  
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+		
+}
